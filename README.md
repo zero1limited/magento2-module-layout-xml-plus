b57b256762d727d7a199ad246d56ceb29732ed97
adamzero1 <adam.crossland@zero1.co.uk>
2024-04-29 08:11:01 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Zero1_LayoutXmlPlus
+
---
+++ registration.php
@@ -0,0 +1,7 @@
+<?php
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Zero1_LayoutXmlPlus',
+    __DIR__
+);
