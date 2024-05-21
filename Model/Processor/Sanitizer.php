7530bdb672869eef147b0dec8f1294a7625f80d5
adamzero1 <adam.crossland@zero1.co.uk>
2024-05-21 08:56:32 +0000

    stop & from being html entitied

--- Model/Processor/Sanitizer.php
+++ Model/Processor/Sanitizer.php
@@ -3,18 +3,20 @@ namespace Zero1\LayoutXmlPlus\Model\Processor;
 
 class Sanitizer
 {
-    public const TEMPLATE_AT = 'atatatat';
+    public const TEMPLATE_AT = '___at___';
+    public const TEMPLATE_AMPERSAND = '___amp___';
 
     public function sanitize($value)
     {
         $value = str_replace('@', self::TEMPLATE_AT, $value);
+        $value = str_replace('&', self::TEMPLATE_AMPERSAND, $value);
         return $value;
     }
 
     public function unsanitize($value)
     {
         $value = str_replace(self::TEMPLATE_AT, '@', $value);
+        $value = str_replace(self::TEMPLATE_AMPERSAND, '&', $value);
         return $value;
     }
 }
-
