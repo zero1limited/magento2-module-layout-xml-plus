67e21720dfc3612882f7d291187dc381b471e0ff
callum-zero1 <callum.breeze@zero1.co.uk>
2025-01-20 14:17:45 +0000

    Add pound sign sanitization

--- Model/Processor/Sanitizer.php
+++ Model/Processor/Sanitizer.php
@@ -6,12 +6,14 @@ class Sanitizer
     public const TEMPLATE_AT = '___at___';
     public const TEMPLATE_AMPERSAND = '___amp___';
     public const TEMPLATE_ESI_INCLUDE = 'esi___include';
+    public const TEMPLATE_POUND = '__pound__';
 
     public function sanitize($value)
     {
         $value = str_replace('@', self::TEMPLATE_AT, $value);
         $value = str_replace('&', self::TEMPLATE_AMPERSAND, $value);
         $value = str_replace('esi:include', self::TEMPLATE_ESI_INCLUDE, $value);
+        $value = str_replace('£', self::TEMPLATE_POUND, $value);
         return $value;
     }
 
@@ -20,6 +22,7 @@ class Sanitizer
         $value = str_replace(self::TEMPLATE_AT, '@', $value);
         $value = str_replace(self::TEMPLATE_AMPERSAND, '&', $value);
         $value = str_replace(self::TEMPLATE_ESI_INCLUDE, 'esi:include', $value);
+        $value = str_replace(self::TEMPLATE_POUND, '£', $value);
         return $value;
     }
 }
