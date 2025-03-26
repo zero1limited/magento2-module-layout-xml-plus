d72a7979327579830e6ccd3dc2b5bad91b5fd4a0
adamzero1 <adam.crossland@zero1.co.uk>
2025-03-26 07:51:54 +0000

    dev: work on module

--- Model/Processor/Sanitizer.php
+++ Model/Processor/Sanitizer.php
@@ -3,26 +3,35 @@ namespace Zero1\LayoutXmlPlus\Model\Processor;
 
 class Sanitizer
 {
-    public const TEMPLATE_AT = '___at___';
-    public const TEMPLATE_AMPERSAND = '___amp___';
-    public const TEMPLATE_ESI_INCLUDE = 'esi___include';
-    public const TEMPLATE_POUND = '__pound__';
+    public const SEARCH = [
+        '@',
+        '&',
+        'esi:include',
+        '£'
+    ];
+
+    public const REPLACE = [
+        '___at___',
+        '___amp___',
+        'esi___include',
+        '__pound__',
+    ];
 
     public function sanitize($value)
     {
-        $value = str_replace('@', self::TEMPLATE_AT, $value);
-        $value = str_replace('&', self::TEMPLATE_AMPERSAND, $value);
-        $value = str_replace('esi:include', self::TEMPLATE_ESI_INCLUDE, $value);
-        $value = str_replace('£', self::TEMPLATE_POUND, $value);
-        return $value;
+        return str_replace(
+            self::SEARCH,
+            self::REPLACE,
+            $value
+        );
     }
 
     public function unsanitize($value)
     {
-        $value = str_replace(self::TEMPLATE_AT, '@', $value);
-        $value = str_replace(self::TEMPLATE_AMPERSAND, '&', $value);
-        $value = str_replace(self::TEMPLATE_ESI_INCLUDE, 'esi:include', $value);
-        $value = str_replace(self::TEMPLATE_POUND, '£', $value);
-        return $value;
+        return str_replace(
+            self::REPLACE,
+            self::SEARCH,
+            $value
+        );
     }
 }
